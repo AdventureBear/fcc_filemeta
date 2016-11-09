204a81454d4eef514900456d46a5d64db995a1f0
adventurebear <suzanneatkinson@gmail.com>
2016-11-08 23:56:47 -0500

    Working

--- server.js
+++ server.js
@@ -12,6 +12,8 @@
 var express = require('express');
 var mongodb = require('mongodb');
 var dotenv = require('dotenv');
+var multer  = require('multer');
+var upload = multer();
 
 
 var app = express();
@@ -34,11 +36,23 @@ MongoClient.connect(url, function (err, db) {
   } else {
     console.log('Connection established to database');
 
+
     // do some work here with the database.
     app.get('/', function (req, res) {
       res.send(index.html);
     });
 
+
+    app.post('/', upload.single('fileupload'), function (req, res) {
+        var size = req.file.size;
+        console.log(size);
+        //res.sendStatus(size);
+        // req.file is the `avatar` file
+        // req.body will hold the text fields, if there were any
+      output = {size: size};
+      res.send(output);
+      //res.end();
+    })
 
     //Create the server connection
     app.listen(app.get('port'), function () {
